b8229eda888093a28318d2e98b6e35789b6950a3
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2023-06-11 18:20:04 +0300

    add:room entity

---
+++ lib/core/domain/entity/room.dart
@@ -0,0 +1,23 @@
+class Room {
+  final int number;
+  final int capacity;
+  final String type;
+  final List<String> equipment;
+  final int price;
+
+  Room({
+    required this.number,
+    required this.capacity,
+    required this.type,
+    required this.equipment,
+    required this.price,
+  });
+
+  factory Room.fromJson(Map<String, dynamic> json) => Room(
+    number: json['number'],
+    capacity: json['capacity'],
+    equipment: json['equipment'],
+    type: json['type'],
+    price: json['price'],
+  );
+}
