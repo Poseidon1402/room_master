8992c9e96b0375b45f4693e200d77b2488af7ad4
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2023-06-30 15:25:30 +0300

    add:reservations fetching

--- lib/core/data/repository/reservation_repository.dart
+++ lib/core/data/repository/reservation_repository.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../../domain/entity/reservation.dart';
+
 class ReservationRepository {
   final _collection = FirebaseFirestore.instance.collection('reservations');
 
@@ -8,4 +10,15 @@ class ReservationRepository {
     return newReservation.id;
   }
 
+  Future<List<Reservation>> getReservations() async {
+    final docs = (await _collection.get()).docs;
+
+    final List<Reservation> reservations = [];
+
+    for (var doc in docs) {
+      reservations.add(Reservation.fromJson(doc.data()));
+    }
+
+    return reservations;
+  }
 }
